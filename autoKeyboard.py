5bbab7b1b3a7cf82d4c7209798a38d5e976548d7
r00t0ver <tanvir.22464751@gmail.com>
2025-01-09 19:12:31 +0600

    first commit

---
+++ autoKeyboard.py
@@ -0,0 +1,73 @@
+import pyautogui
+import time
+#wait for 3 second
+#clicked on the surface of operation area
+time.sleep(3)
+count = input("Enter the number of time you want to run:" )
+for i in range(int(count)):
+    for _ in range(3):
+        pyautogui.press('tab')
+        time.sleep(0.2)
+
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(3):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(3):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(3):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(3):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(3):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    pyautogui.press('tab')
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)
+    for _ in range(2):
+        pyautogui.press('tab')
+        time.sleep(0.2)
+    for _ in range(2):
+        pyautogui.press('down')
+        time.sleep(0.2)  
+    for _ in range(2):
+        pyautogui.press('tab')
+        time.sleep(0.2)
+    pyautogui.press('enter')   
+    time.sleep(1)
+    pyautogui.press('tab')  
+    time.sleep(0.2)
+    pyautogui.press('enter')   
+    time.sleep(4)
